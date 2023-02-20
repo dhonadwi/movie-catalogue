f428bc23ef31f310e5362aec144958ebca7f9c30
dhonadwi <dhonadwi2@gmail.com>
2023-02-20 10:12:40 +0700

    add spinner.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import 'regenerator-runtime';
 import '../styles/style.css';
 import '../styles/responsive.css';
+import '../../node_modules/spin.js/spin.css';
 import App from './views/app';
 import swRegister from './utils/sw-register';
 import webSocketInitiator from './utils/webSocket-initiator';
--- src/scripts/views/pages/Login.js
+++ src/scripts/views/pages/Login.js
@@ -2,11 +2,13 @@ import ApiSource from '../../data/api';
 import Users from '../../utils/users';
 import App from '../app';
 import Swal from 'sweetalert2';
+import { Spinner } from 'spin.js';
 
 const Login = {
   async render() {
     return `
     <div class="content">
+    <div id="spinner"></div>
     <h2 class="content__heading">Halaman Login</h2>
     <div id="movies" class="movies">
       <form action="#">
@@ -19,10 +21,40 @@ const Login = {
     `;
   },
   async afterRender() {
+    const spinner = new Spinner({
+      lines: 10,
+      length: 6,
+      width: 3,
+      radius: 6,
+      color: 'black',
+      animation: 'spinner-line-fade-quick',
+      // lines: 13, // The number of lines to draw
+      // length: 38, // The length of each line
+      // width: 17, // The line thickness
+      // radius: 45, // The radius of the inner circle
+      scale: 1, // Scales overall size of the spinner
+      corners: 1, // Corner roundness (0..1)
+      speed: 1, // Rounds per second
+      rotate: 0, // The rotation offset
+      animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      // color: '#ffffff', // CSS color or array of colors
+      // fadeColor: 'transparent', // CSS color or array of colors
+      // top: '50%', // Top position relative to parent
+      // left: '50%', // Left position relative to parent
+      // shadow: '0 0 1px transparent', // Box-shadow for the lines
+      // // zIndex: 2000000000, // The z-index (defaults to 2e9)
+      className: 'spinner', // The CSS class to assign to the spinner
+      // position: 'absolute', // Element positioning
+    });
+    spinner.spin(document.getElementById('spinner'));
+    document.getElementById('spinner').style.display = 'none';
     const btnSubmit = document.querySelector('#submit');
     const name = document.querySelector('#name');
     const password = document.querySelector('#password');
     btnSubmit.addEventListener('click', async () => {
+      spinner.spin(document.getElementById('spinner'));
+      document.getElementById('spinner').style.display = 'block';
       const data = {
         nama: name.value,
         pass: password.value,
@@ -41,8 +73,12 @@ const Login = {
           content: document.querySelector('#mainContent'),
           tags: document.querySelectorAll('a'),
         });
+        spinner.stop();
+        document.getElementById('spinner').style.display = 'none';
         app.renderPage();
       } else {
+        spinner.stop();
+        document.getElementById('spinner').style.display = 'none';
         Swal.fire({
           title: 'Error!',
           text: 'User not Authorized',
